c48412c9e93f51512fa4420856956320c9862ba7
BereketW <psycheciphers@gmail.com>
2024-05-31 10:20:10 -0700

    something

--- apps/server/webpack.config.cjs
+++ apps/server/webpack.config.cjs
@@ -1,20 +1,21 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-const path = require('path');
-const nodeExternals = require('webpack-node-externals');
-const isProduction = process.env.NODE_ENV === 'production';
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+const isProduction = process.env.NODE_ENV === "production";
 
-const { spawn } = require('child_process');
+const { spawn } = require("child_process");
+const { experiments } = require("webpack");
 
 function OnFirstBuildDonePlugin() {
   let isInitialBuild = true;
   return {
     apply: (compiler) => {
-      compiler.hooks.done.tap('OnFirstBuildDonePlugin', (compilation) => {
+      compiler.hooks.done.tap("OnFirstBuildDonePlugin", (compilation) => {
         if (isInitialBuild) {
           isInitialBuild = false;
-          spawn('nodemon dist/main.js --watch dist', {
-            stdio: 'inherit',
+          spawn("nodemon dist/main.js --watch dist", {
+            stdio: "inherit",
             shell: true,
           });
         }
@@ -24,34 +25,41 @@ function OnFirstBuildDonePlugin() {
 }
 
 const config = {
-  entry: './src/main.js',
+  entry: "./src/main.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, "dist"),
     clean: true,
   },
-  target: 'node',
+  target: "node",
   externals: [nodeExternals()],
   resolve: {
     alias: {
-      '@/prisma/client': path.resolve(__dirname, './prisma/client'),
+      "@/prisma/client": path.resolve(__dirname, "./prisma/client"),
     },
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/i,
-        loader: 'babel-loader',
+        loader: "babel-loader",
+      },
+      {
+        test: /\.wasm$/,
+        type: "webassembly/async",
       },
     ],
+  },
+  experiments: {
+    asyncWebAssembly: true,
   },
 };
 
 module.exports = () => {
   if (isProduction) {
     config.devtool = false;
-    config.mode = 'production';
+    config.mode = "production";
   } else {
-    config.mode = 'development';
+    config.mode = "development";
     config.plugins = [OnFirstBuildDonePlugin()];
   }
   return config;
